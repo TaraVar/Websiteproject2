63388b4442d3517e30ef6b53517abe491c29efb4
TaraVar <tara.vartio@gmail.com>
2026-02-18 20:18:12 +0200

    Update paivitanocco.php

--- paivitanocco.php
+++ paivitanocco.php
@@ -1,66 +1,82 @@
 <?php
-
 $json = isset($_POST["nocco"]) ? $_POST["nocco"] : "";
 
-if (!($nocco = tarkistaJson($json))){
+if (!($nocco = tarkistaJson($json))) {
     print "Täytä kaikki kentät";
     exit;
 }
 
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 
-try{
+try {
     $yhteys = mysqli_connect("db", "root", "password", "nocco");
-}
-catch(Exception $e){
+} catch (Exception $e) {
     print "Yhteysvirhe";
     exit;
 }
 
+$taulu = $nocco->taulu;
+
+if ($taulu === "bcaa") {
+    mysqli_query($yhteys,
+        "UPDATE bcaa SET 
+            maku='$nocco->maku',
+            kuvaus='$nocco->kuvaus',
+            kuva='$nocco->kuva'
+         WHERE id=$nocco->id"
+    );
+}
+
+if ($taulu === "focus") {
+    mysqli_query($yhteys,
+        "UPDATE focus SET 
+            maku='$nocco->maku',
+            kuvaus='$nocco->kuvaus',
+            kuva='$nocco->kuva'
+         WHERE id=$nocco->id"
+    );
+}
+
+
+// BCAA
+$bcaa = [];
 $tulos = mysqli_query($yhteys, "SELECT * FROM bcaa");
-
 while ($rivi = mysqli_fetch_object($tulos)) {
-    $Nocco = new class{};
-    $Nocco->id = $rivi->id;
-    $Nocco->maku = $rivi->maku;
-    $Nocco->kuvaus = $rivi->kuvaus;
-    $Nocco->kuva = $rivi->kuva;
-
-    $bcaa[] = $Nocco;
+    $bcaa[] = $rivi;
 }
 
+// FOCUS
+$focus = [];
 $tulos = mysqli_query($yhteys, "SELECT * FROM focus");
-
 while ($rivi = mysqli_fetch_object($tulos)) {
-    $Nocco = new class{};
-    $Nocco->id = $rivi->id;
-    $Nocco->maku = $rivi->maku;
-    $Nocco->kuvaus = $rivi->kuvaus;
-    $Nocco->kuva = $rivi->kuva;
-
-    $focus[] = $Nocco;
+    $focus[] = $rivi;
 }
 
-mysqli_close($yhteys)
+mysqli_close($yhteys);
+
 print json_encode([
     "bcaa" => $bcaa,
     "focus" => $focus
 ]);
 
-?>
-
-<?php
-function tarkistaJson($json){
-    if (empty($json)){
+function tarkistaJson($json) {
+    if (empty($json)) {
         return false;
     }
 
     $nocco = json_decode($json, false);
 
-    if (empty($nocco->id)||empty($nocco->maku)||empty($nocco->kuvaus)||empty($nocco->kuva)) {
+    if (
+        empty($nocco->id) ||
+        empty($nocco->maku) ||
+        empty($nocco->kuvaus) ||
+        empty($nocco->kuva) ||
+        empty($nocco->taulu)   
+    ) {
         return false;
     }
 
     return $nocco;
 }
+
 ?>
