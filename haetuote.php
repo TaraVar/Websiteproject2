e0d58cbf81ec1ffd798012e81d65d82e221c6582
TaraVar <tara.vartio@gmail.com>
2026-02-18 19:48:07 +0200

    Create haetuote.php

---
+++ haetuote.php
@@ -0,0 +1,14 @@
+<?php
+$id = $_GET["id"];
+$taulu = $_GET["taulu"]; // bcaa tai focus
+
+$yhteys = mysqli_connect("db", "root", "password", "nocco");
+
+$sql = "SELECT * FROM $taulu WHERE id=$id";
+$tulos = mysqli_query($yhteys, $sql);
+$rivi = mysqli_fetch_object($tulos);
+
+echo "<h2>$rivi->maku</h2>";
+echo "<p>$rivi->kuvaus</p>";
+echo "<img src='Images/$rivi->kuva' width='200'>";
+?>
