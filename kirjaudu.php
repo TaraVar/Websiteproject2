63f1e931275215c20e0d8944e055bc8fffeb5703
TaraVar <tara.vartio@gmail.com>
2026-02-19 21:39:56 +0200

    Create kirjaudu.php

---
+++ kirjaudu.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+$yhteys = mysqli_connect("db", "root", "password", "nocco");
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$tulos = mysqli_query($yhteys, "SELECT * FROM users WHERE username='$username'");
+$user = mysqli_fetch_object($tulos);
+
+if ($user && password_verify($password, $user->password_hash)) {
+
+    $_SESSION["user_id"] = $user->id;
+    $_SESSION["username"] = $user->username;
+    $_SESSION["role"] = $user->role;
+
+    if ($user->role === "admin") {
+        header("Location: admin.php");
+    } else {
+        header("Location: index.html");
+    }
+    exit;
+
+} else {
+    echo "Väärä tunnus tai salasana";
+}
+?>
